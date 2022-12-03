6c596faf5d14c06621164cf57af571c294ab8210
Eric Bugarin <ebugarin402@gmail.com>
2022-12-03 17:42:39 -0600

    working on landing

--- food-team-recipe-app/src/components/Landing.js
+++ food-team-recipe-app/src/components/Landing.js
@@ -8,6 +8,7 @@ import {CheckCircle, Help } from '@mui/icons-material';
 
 function Landing() {
   const [authorizationCode, setAuthorizationCode] = useState('');
+  const [zipCode, setZipCode] = useState('');
   const initialData = InitialData.hits
   useEffect(() => {
     const queryParams = new URLSearchParams(window.location.search);
@@ -24,30 +25,34 @@ function Landing() {
         <>
           <p> Welcome to the recipes app. </p>
           <a className="App-link" href="https://github.com/ckclassrooms/final-project-proposal-food-team" target="_blank" rel="noopener noreferrer"> Repository </a>
-          <br/> <br/> <SearchBar authCode={authorizationCode} /> <br/> 
-          <div id="loadedlist">
-            <h2> Some recipes to try </h2>
-              <ul>
-                <li> <Link to="/RecipeInfo" replace={true} state = {{recipe:initialData[0].recipe, auth:authorizationCode}}>Classic Taco Salad</Link> </li>
-                <li> <Link to="/RecipeInfo" replace={true} state = {{recipe:initialData[1].recipe, auth:authorizationCode}}>Vegetarian Taco Chili</Link> </li>
-                <li> <Link to="/RecipeInfo" replace={true} state = {{recipe:initialData[2].recipe, auth:authorizationCode}}>Taco Stuffed Burger</Link> </li>
-                <li> <Link to="/RecipeInfo" replace={true} state = {{recipe:initialData[3].recipe, auth:authorizationCode}}>Turkey chilli & rice tacos</Link> </li>
-                <li> <Link to="/RecipeInfo" replace={true} state = {{recipe:initialData[9].recipe, auth:authorizationCode}}>Beef Taco Roll-Ups</Link> </li>
-                <li> <Link to="/RecipeInfo" replace={true} state = {{recipe:initialData[5].recipe, auth:authorizationCode}}>Taco Chili Soup</Link> </li>
-              </ul>            
-          </div>
-          <div>
-            {!authorizationCode && <Stack direction="row" spacing={60}>
-              <Button onClick={ () => /* https://starlit-twilight-fde55f.netlify.app/ http://localhost:3000/ */ window.location.href = 'https://api.kroger.com/v1/connect/oauth2/authorize?scope=cart.basic:write&response_type=code&client_id=foodappforschool-043e65debc535226ffbd8fa7ed03f8041525245609739767566&redirect_uri=https://starlit-twilight-fde55f.netlify.app/'  } variant="contained" startIcon={<Help />} color="error" >
-                If you want to add ingredients to your Kroger cart, sign in here.
-              </Button>
-            </Stack>}
-            {authorizationCode && <Stack direction="row" spacing={60}>
-              <Button onClick={ () => null  } variant="contained" startIcon={<CheckCircle />} color="success" >
-                You can now add ingredients to your Kroger cart!
-              </Button>
-            </Stack>}
-          </div>
+          { zipCode &&
+          <>
+            <br/> <br/> <SearchBar authCode={authorizationCode} /> <br/> 
+            <div id="loadedlist">
+              <h2> Some recipes to try </h2>
+                <ul>
+                  <li> <Link to="/RecipeInfo" replace={true} state = {{recipe:initialData[0].recipe, auth:authorizationCode}}>Classic Taco Salad</Link> </li>
+                  <li> <Link to="/RecipeInfo" replace={true} state = {{recipe:initialData[1].recipe, auth:authorizationCode}}>Vegetarian Taco Chili</Link> </li>
+                  <li> <Link to="/RecipeInfo" replace={true} state = {{recipe:initialData[2].recipe, auth:authorizationCode}}>Taco Stuffed Burger</Link> </li>
+                  <li> <Link to="/RecipeInfo" replace={true} state = {{recipe:initialData[3].recipe, auth:authorizationCode}}>Turkey chilli & rice tacos</Link> </li>
+                  <li> <Link to="/RecipeInfo" replace={true} state = {{recipe:initialData[9].recipe, auth:authorizationCode}}>Beef Taco Roll-Ups</Link> </li>
+                  <li> <Link to="/RecipeInfo" replace={true} state = {{recipe:initialData[5].recipe, auth:authorizationCode}}>Taco Chili Soup</Link> </li>
+                </ul>            
+            </div>
+            <div>
+              {!authorizationCode && <Stack direction="row" spacing={60}>
+                <Button onClick={ () => /* https://starlit-twilight-fde55f.netlify.app/ http://localhost:3000/ */ window.location.href = 'https://api.kroger.com/v1/connect/oauth2/authorize?scope=cart.basic:write&response_type=code&client_id=foodappforschool-043e65debc535226ffbd8fa7ed03f8041525245609739767566&redirect_uri=https://starlit-twilight-fde55f.netlify.app/'  } variant="contained" startIcon={<Help />} color="error" >
+                  If you want to add ingredients to your Kroger cart, sign in here.
+                </Button>
+              </Stack>}
+              {authorizationCode && <Stack direction="row" spacing={60}>
+                <Button onClick={ () => null  } variant="contained" startIcon={<CheckCircle />} color="success" >
+                  You can now add ingredients to your Kroger cart!
+                </Button>
+              </Stack>}
+            </div>
+          </>
+          }
         </>
     )
 }
